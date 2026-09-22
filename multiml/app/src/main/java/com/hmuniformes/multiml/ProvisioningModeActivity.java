292c99534395a636f833876bafcc03022732fe8f
Eusebio Hernandez Hernandez <134029779+hernandez87-byte@users.noreply.github.com>
2026-09-21 22:35:01 -0600

    Multi ML Android build source

---
+++ multiml/app/src/main/java/com/hmuniformes/multiml/ProvisioningModeActivity.java
@@ -0,0 +1,28 @@
+package com.hmuniformes.multiml;
+
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class ProvisioningModeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String action = getIntent() == null ? null : getIntent().getAction();
+
+        if (DevicePolicyManager.ACTION_GET_PROVISIONING_MODE.equals(action)) {
+            Intent result = new Intent();
+            result.putExtra(
+                    DevicePolicyManager.EXTRA_PROVISIONING_MODE,
+                    DevicePolicyManager.PROVISIONING_MODE_MANAGED_PROFILE
+            );
+            setResult(RESULT_OK, result);
+        } else if (DevicePolicyManager.ACTION_ADMIN_POLICY_COMPLIANCE.equals(action)) {
+            setResult(RESULT_OK);
+        } else {
+            setResult(RESULT_CANCELED);
+        }
+        finish();
+    }
+}
